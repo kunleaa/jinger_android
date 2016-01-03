993254f0420f09c07ada1ae5e5d3868c3cf84d1b
kunleaa <445241750@qq.com>
2016-01-03 22:54:24 +0800

    修改简单导航的小bug

--- OnDraw/src/com/OnDraw/Simple_Navigation.java
+++ OnDraw/src/com/OnDraw/Simple_Navigation.java
@@ -1,15 +1,17 @@
 package com.OnDraw;
-public class Simple_Navigation{
+public class Simple_Navigation extends RotateAndFilt{
 	PeakFinder PeFin = new PeakFinder();
 	StepDistCalculater SDCal = new StepDistCalculater();
 	float angleTrans = 0;
 	void run_simple_navigate(Data_Sensor ds, DrawView dv, Config cfg)
 	{
-		float[] data_ori_tans = ds.use_ori_trans();
-		float[] data_acc = ds.use_acc();
+		float [] data_ori_tans = ds.use_ori_trans();
+		float [] AbsCoodinate_filt;
+		//由旋转矩阵计算的绝对坐标系下加速度的坐标值 
+		AbsCoodinate_filt = rotate_filt(ds,dv);
 		angleTrans = data_ori_tans[0];
 		//取极大值和极小值，加负号去掉方向的影响
-		PeFin.FindPeak(-data_acc[2]);
+		PeFin.FindPeak(-AbsCoodinate_filt[2]);
 		SDCal.CalcuStepDist(PeFin,cfg.STEP_PARAM);
 		if(SDCal.isStep == true)
 		{
